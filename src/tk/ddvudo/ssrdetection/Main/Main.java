0d63003f5e299064656cff67f4923f72f70c602b
ddvudo <644077730@qq.com>
2019-01-08 12:02:19 +0800

    重新组织代码，增加链接判空，增加单线程测试时耗时计算

--- src/tk/ddvudo/ssrdetection/Main/Main.java
+++ src/tk/ddvudo/ssrdetection/Main/Main.java
@@ -1,13 +1,12 @@
 package tk.ddvudo.ssrdetection.Main;
 
 import java.net.URLConnection;
+
+import org.apache.commons.lang.StringUtils;
+
 import tk.ddvudo.ssrdetection.Utils.URLHandler.URLConnHandler;
 import tk.ddvudo.ssrdetection.Utils.URLHandler.URLIOHandler;
 import tk.ddvudo.ssrdetection.Utils.dataResolve.DataResolve;
-import tk.ddvudo.ssrdetection.Utils.netHadler.jPingy.Ping;
-import tk.ddvudo.ssrdetection.Utils.netHadler.jPingy.PingArguments;
-import tk.ddvudo.ssrdetection.Utils.netHadler.jPingy.PingResult;
-import tk.ddvudo.ssrdetection.Utils.netHadler.jPingy.Ping.Backend;
 import tk.ddvudo.ssrdetection.beans.Server;
 import tk.ddvudo.ssrdetection.beans.airportdata;
 
@@ -18,19 +17,13 @@ public class Main {
 		try {
 			iohandler = URLIOHandler.getInstance();
 			String linkurl = iohandler.getInputUrl();
-			if(null == linkurl || linkurl.equals("")) {
+			if(StringUtils.isEmpty(linkurl)) {
 				System.out.println("链接为空，退出");
+				return;
 			}
 			URLConnection con = URLConnHandler.getInstance(linkurl).getConnection();
 			airportdata data = DataResolve.getInstance().decode(iohandler.getResponseContent(con));
-			for(Server s : data.getServers()) {
-				System.out.println(">>>>>>开始测试"+s.getId()+"----->"+s.getRemarks());
-				PingArguments arguments = new PingArguments.Builder().url(s.getServer()).timeout(500).count(2).bytes(32).build();
-				PingResult results = Ping.ping(arguments, Backend.WINDOWS_zhCN);
-				if(results.rtt_avg()>0) {
-					System.out.println(s.getRemarks()+"-------测试结果"+results.rtt_avg()+"ms");
-				}
-			}
+			DataResolve.getInstance().serverPingTestSingleThread(data.getServers().toArray(new Server[data.getServers().size()]));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
--- src/tk/ddvudo/ssrdetection/Utils/dataResolve/DataResolve.java
+++ src/tk/ddvudo/ssrdetection/Utils/dataResolve/DataResolve.java
@@ -8,6 +8,11 @@ import org.apache.commons.codec.binary.Base64;
 
 import com.alibaba.fastjson.JSON;
 
+import tk.ddvudo.ssrdetection.Utils.netHadler.jPingy.Ping;
+import tk.ddvudo.ssrdetection.Utils.netHadler.jPingy.PingArguments;
+import tk.ddvudo.ssrdetection.Utils.netHadler.jPingy.PingResult;
+import tk.ddvudo.ssrdetection.Utils.netHadler.jPingy.Ping.Backend;
+import tk.ddvudo.ssrdetection.beans.Server;
 import tk.ddvudo.ssrdetection.beans.airportdata;
 
 public class DataResolve {
@@ -19,7 +24,7 @@ public class DataResolve {
 		return (airportdata) JSON.parseObject(decoded, airportdata.class);
 	}
 
-	public static String unicodeToString(String str) {
+	public String unicodeToString(String str) {
 		Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
 		Matcher matcher = pattern.matcher(str);
 		char ch;
@@ -31,7 +36,21 @@ public class DataResolve {
 		}
 		return str;
 	}
-
+	
+	public void serverPingTestSingleThread(Server... servers) {
+		long t1 = System.currentTimeMillis();
+		for(Server s : servers) {
+			System.out.println("开始测试"+s.getId()+"----->"+s.getRemarks());
+			PingArguments arguments = new PingArguments.Builder().url(s.getServer()).timeout(500).count(2).bytes(32).build();
+			PingResult results = Ping.ping(arguments, Backend.WINDOWS_zhCN);
+			if(results.rtt_avg()>0) {
+				System.out.println(s.getRemarks()+"测试结果"+results.rtt_avg()+"ms");
+			}
+		}
+		long t2 = System.currentTimeMillis();
+		System.out.println("测试结束,耗时"+(t2-t1)+"ms");
+	}
+	
 	public static final DataResolve getInstance() {
 		return new DataResolve();
 	}
