76d3f8739e0116c1e59f5838f058189a00a87712
ddvudo <644077730@qq.com>
2019-01-11 16:42:25 +0800

    异常机制修正
    Signed-off-by: ddvudo <644077730@qq.com>

--- src/tk/ddvudo/ssrdetection/Utils/URLHandler/URLIOHandler.java
+++ src/tk/ddvudo/ssrdetection/Utils/URLHandler/URLIOHandler.java
@@ -3,7 +3,6 @@ package tk.ddvudo.ssrdetection.Utils.URLHandler;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.SocketTimeoutException;
 import java.net.URLConnection;
 
 public class URLIOHandler {
