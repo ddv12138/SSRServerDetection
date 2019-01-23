3853e77260311e57d9cdd9650183699357d8b099
ddv12138 <644077730@qq.com>
2019-01-23 21:54:51 +0800

    skrskr
    Signed-off-by: ddv12138 <644077730@qq.com>

--- src/tk/ddvudo/ssrdetection/Utils/netHadler/NetReach/NetReachable.java
+++ src/tk/ddvudo/ssrdetection/Utils/netHadler/NetReach/NetReachable.java
@@ -11,15 +11,16 @@ public class NetReachable {
 	public static void main(String... args) {
 		 try {
 			ParallelClient pc = new ParallelClient();
-			pc.preparePing().setTargetHostsFromString("lisuanlaoji.co").execute(new ParallecResponseHandler() {
+			long a = pc.preparePing().setTargetHostsFromString("lisuanlaoji.co").execute(new ParallecResponseHandler() {
 				
 				@Override
 				public void onCompleted(ResponseOnSingleTask res, Map<String, Object> responseContext) {
 					Global.getInstance().getLogger().info("ddv");
 					Global.getInstance().getLogger().info(res.getHost()+"-------->"+res.getOperationTimeMillis());
 				}
-			});
+			}).getSubmitTime();
 			pc.releaseExternalResources();
+			Global.getInstance().getLogger().info("ddv----"+a);
 //			InetAddress address = InetAddress.getByName("www.bilibili.com");
 //			Global.getInstance().getLogger().info(address.isReachable(5000));
 		} catch (Exception e) {
